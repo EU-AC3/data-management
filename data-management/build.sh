4e88a28e59957a2f912af0436ed8e91a009bb5e2
amaxilat <d.amaxilatis@gmail.com>
2025-12-23 22:03:29 +0200

    update broker definitions and docker image build procedure

--- data-management/build.sh
+++ data-management/build.sh
@@ -1,1 +1,1 @@
-docker buildx build --platform linux/arm/v7,linux/arm64/v8,linux/amd64 . -f rabbit/Dockerfile -t sparkworks/ac3-edge-broker:0.3 --push
+docker buildx build --platform linux/arm/v7,linux/arm64/v8,linux/amd64 . -f rabbit/Dockerfile -t sparkworks/ac3-edge-broker:0.5 --push
